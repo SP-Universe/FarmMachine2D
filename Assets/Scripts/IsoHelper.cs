4af74eb687dec74b319d555b94a47e08c0df2a12
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-01-05 13:35:23 +0100

    [ADDED] Project

---
+++ Assets/Scripts/IsoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoHelper : MonoBehaviour
+{
+    public Vector3 IsoToXY(Vector3 v)
+    {
+        return new Vector3(0.5f * v.x - v.y, 0.5f * v.x + v.y, 0);
+    }
+
+    public Vector3 XYToIso(Vector3 v)
+    {
+        return new Vector3(v.x + v.y, 0.5f * (v.y - v.x),0);
+    }
+}
